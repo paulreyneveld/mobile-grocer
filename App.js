af8978af036532f3d645a05015ffdc48c4c575d6
Paul Reyneveld <paulreyneveld@Pauls-MacBook-Pro-2.local>
2022-03-21 21:19:31 -0700

    Created front end functionality: able to add items to an array and display them

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
 
 export default function App() {
+
+  const [item, setItem] = useState('')
+  const [list, setList] = useState([])
+
+  console.log(list)
+
+  const handleAddItem = () => {
+    setList(list.concat(item))
+    setItem('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Bae, what do we need?</Text>
+      <TextInput 
+        style={styles.input}
+        value={item}
+        onChangeText={ (text) => setItem(text)}
+
+      />
+      <Button 
+        title="Add item"
+        onPress={handleAddItem}
+      />
+      <Text>{item}</Text>
+      {list.map((item, index) => 
+        <Text key={index}
+              onPress={() => console.log('hell')}
+        >
+          {item}
+        </Text>
+      )}
     </View>
   );
 }
@@ -13,8 +42,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    marginTop: 20
   },
+  input: {
+    height: 40, 
+    borderColor: 'black', 
+    borderWidth: 1,
+    marginBottom: 5
+  }
 });
